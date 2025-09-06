1618aa725fad592fceac84e71f5d5c6bba828866
daviddl0311 <daviddominguezladera@gmail.com>
2025-09-05 21:31:18 -0500

    Update Seo

--- js/app.js
+++ js/app.js
@@ -4,11 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
         const fecha = document.querySelector("#date");
         const hora = document.querySelector("#hour");
         // const dias = ["DOM","LUN","MAR","MIE","JUE","VIE","SAB"];
+
         let date = new Date();
+        let myDay = date.toLocaleDateString(undefined, {weekday: 'long'});
+        let myDate = date.toLocaleDateString(undefined, {month: 'long'});
 
         // dia.textContent = dias[date.getDay()];
-        dia.textContent = date.toLocaleDateString(undefined, {weekday: 'long'}).toUpperCase();
-        fecha.textContent = `${date.getDate().toString().padStart(2,'0')} de ${date.toLocaleDateString(undefined, {month: 'long'})} de ${date.getFullYear()}`;
+        dia.textContent = myDay.toUpperCase();
+        fecha.textContent = `${date.getDate().toString().padStart(2,'0')} de ${myDate.slice(0, 1).toUpperCase().concat(myDate.slice(1))} de ${date.getFullYear()}`;
         hora.textContent = `${date.getHours().toString().padStart(2,'0')}:${date.getMinutes().toString().padStart(2,'0')}:${date.getSeconds().toString().padStart(2,'0')}`;
     }
 
